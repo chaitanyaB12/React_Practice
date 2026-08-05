1d64f1573b2d344d28f7bb5dfe3f294247209c30
Chaitanya <chaitanyabhopnikar@gmail.com>
2026-08-05 23:12:04 +0530

    basic form project 1st stage used useReducer Hook

--- project9_BasicForm/src/component/BasicForm.jsx
+++ project9_BasicForm/src/component/BasicForm.jsx
@@ -1,8 +1,31 @@
- 
+import { useReducer } from "react";
+
+    
 
  const BasicForm = ()=>{
+
+    const BasicInfo ={
+        name:"",
+        email:"",
+        age:"",
+        gender:"",
+        city:""
+    }
+
+    const [action, payload] = useReducer(reducer, BasicInfo)
+
+  
+
     return(
-        <div>Hellio</div>
+       <form action="">
+        <div>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+            <p></p>
+        </div>
+       </form>
     )
  }
  export default BasicForm;
